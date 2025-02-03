d965f29dbd4308d3c389fa2562c4777f2a81fe37
Duc Thinh <thinhtd2109@gmail.com>
2025-02-03 18:41:13 +0700

    update requirements

--- trading-fx.js
+++ trading-fx.js
@@ -5,12 +5,11 @@ import cron from 'node-cron'
 import delay from "delay";
 dotenv.config();
 
-const url = `http://localhost:5001/get-open`;
+const PREDICT_URI = process.env.ENV == 'LOCAL' ? 'http://localhost:5001' : process.env.PREDICT_URI;
 
-let current_close = 0;
 async function tradingv2(count, times) {
     let lot = 0.01
-    const url = `http://localhost:5001/predict`;
+    const url = `${PREDICT_URI}/predict`;
 
     let predict = await axios.post(url, { start: 1 });
 
@@ -19,48 +18,20 @@ async function tradingv2(count, times) {
         side = 'BUY'
     }
 
-    // if (count_decrease == count_increase) {
-    //     await axios.post(`http://localhost:5001/close-current-position`, { lot });
-    //     return true;
-    // };
-
-
-    await axios.post(`http://localhost:5001/close-position`, {
+    await axios.post(`${PREDICT_URI}/close-position`, {
         lot,
         side
     });
  
-    let order = await axios.post(`http://localhost:5001/trade`, {
+    let order = await axios.post(`${PREDICT_URI}/trade`, {
         lot,
         side,
     });
 
 
-    // if (order.data.position_id) {
-    //     await axios.post(`http://localhost:5001/update-position`, {
-    //         position_id: order.data.position_id,
-    //         side,
-    //     });
-    // }
-
    
-    return true;
-    //return bos_choch.data      
+    return true; 
 
-}
-
-async function runMultipleTimes(interval, times, action) {
-    let count = 0;
-
-    async function run() { 
-        if (count < times) {
-            let isEnd = await action(count, times);
-            count++;
-            setTimeout(run, interval);
-        }
-    }
- 
-    await run();
 }
 
 //await tradingv2()
